930882248053d791bb3e815be438dcaf07e686c0
Amrita Dutta <amrita.dutta1497@gmail.com>
2023-11-06 19:32:55 -0800

    Changed the second exercise name from Easy Going to Get Even

--- answers.js
+++ answers.js
@@ -10,7 +10,7 @@ for (let i=1; i<=20; i++)
 }
 
 //-----------------------
-// Easy Going
+// Get Even
 // -----------------------
 
 // Write a for loop that will log only the even numbers in 0 through 200.
